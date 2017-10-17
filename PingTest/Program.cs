1942dc2b62b509929d2ac01a191d297094c9e8de
Jon Coello <joncoello@gmail.com>
2017-10-17 12:42:02 +0100

    update to .net 4

--- PingTest/Program.cs
+++ PingTest/Program.cs
@@ -19,101 +19,15 @@ namespace PingTest
         
         static void Main(string[] args)
         {
-            // logging
-            log4net.Config.XmlConfigurator.Configure();
 
-            SqlConnection conn = CreateConnection();
-
-            var bgWork = new BackgroundWorker();
-            bgWork.WorkerSupportsCancellation = true;
-            bgWork.DoWork += (s, e) =>
-            {
-                while (!bgWork.CancellationPending)
-                {
-
-                    using (var cmd = new SqlCommand("SELECT * FROM MOCK_DATA", conn))
-                    {
-
-                        var a = new SqlDataAdapter(cmd);
-
-                        var ds = new DataSet();
-
-                        var sw = Stopwatch.StartNew();
-
-                        a.Fill(ds);
-
-                        sw.Stop();
-
-                        var logMessage = "query executed in " + sw.Elapsed.ToString();
-                        Log(logMessage);
-
-                    }
-
-                    Thread.Sleep(5000);
-
-                }
-
-            };
-            bgWork.RunWorkerCompleted += (s, e) =>
-            {
-                if (e.Error != null)
-                {
-                    Log("ERROR: " + e.Error.Message, true);
-                }
-                if (conn.State != ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-                if (e.Error != null)
-                {
-                    conn = CreateConnection();
-                    bgWork.RunWorkerAsync();
-                };
-
-            };
-
-            bgWork.RunWorkerAsync();
+            var client = new PingClient();
+            client.Start();
 
             Console.ReadKey();
-            bgWork.CancelAsync();
 
-            Thread.Sleep(5000);
+            client.Stop();
 
         }
-
-        private static void Log(string logMessage, bool isError = false)
-        {
-            Console.WriteLine(logMessage);
-            if (isError) {
-                Logger.Error(logMessage);
-            }
-            else
-            {
-                Logger.Info(logMessage);
-            }
-        }
-
-        private static SqlConnection CreateConnection()
-        {
-            do
-            {
-                try
-                {
-                    Console.WriteLine("connecting");
-                    //var connectionString = @"server = ACCESS-1303SF2\SQL2014 ; database = ping ; user id = sa ; pwd = Patrick@1";
-                    var connectionString = ConfigurationManager.ConnectionStrings["pingDB"].ConnectionString;
-                    var conn = new SqlConnection(connectionString);
-                    conn.Open();
-                    Console.WriteLine("connection successful");
-                    return conn;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("connection failed");
-                    Thread.Sleep(1000);
-                }
-            } while (true);
-        }
-
+        
     }
 }
